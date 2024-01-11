36d430dffed5fc51d97eb71e38e39f3c54c8f1fd
Parker Williams <143528734+pwilliams-wright@users.noreply.github.com>
2024-01-11 09:35:05 -0500

    incorporating changes from Prits001's pull request

--- src/Beginner/encryption.py
+++ src/Beginner/encryption.py
@@ -1,39 +1,39 @@
 ### Encryption Program ###
-# This is a simple encryption/decryption system using a caesar cipher in Python
+# This is a simple encryption/decryption system using a Caesar cipher in Python
 # In this program, the user is prompted to input a message, a key, and a mode (encrypt or decrypt)
-# The program then outputs the encrypted or decrypted message
-import sys
+# The program then outputs the encrypted or decrypted message, depending on the mode
 
-def code_generator():
+def caesarCipher():
     message = input("Enter message: \n")
     key = input("Enter key (A number between 1 and 26. Positive numbers will work for both encryption and decryption.): \n")
+    try:
+        key = int(key)
+    except:
+        print("Error: please enter a key containing only numbers! No letters, special characters, or spaces are allowed! Returning to start...\n")
+        return caesarCipher()
     mode = input("Enter mode (e/encrypt, d/decrypt): \n").lower()
 
     message = message.upper()
 
     if message == "" or key == "" or mode == "":
         print("Error: please enter a valid message, key, and mode! Returning to start...\n")
-        return code_generator()
+        return caesarCipher()
 
     elif message.isalpha() == False:
         print("Error: please enter a message containing only letters! No numbers, special characters, or spaces are allowed! Returning to start...\n")
-        return code_generator()
-    
-    elif key.isdigit() == False:
-        print("Error: please enter a key containing only numbers! No letters, special characters, or spaces are allowed! Returning to start...\n")
-        return code_generator()
+        return caesarCipher()
 
     elif key < 1:
         print("Error: please enter a number equal to or greater than 1! Returning to start...\n")
-        return code_generator()
+        return caesarCipher()
     
     elif key > 26:
         print("Error: please enter a number equal to or less than 26! Returning to start...\n")
-        return code_generator()
+        return caesarCipher()
     
     elif mode not in ['e', 'd', 'encrypt', 'decrypt']:
         print("Error: please enter either e, encrypt or d, decrypt as the mode! Returning to start...\n")
-        return code_generator()
+        return caesarCipher()
 
     def encrypt():
         nonlocal message
@@ -57,7 +57,7 @@ def code_generator():
         encryptedMessage = "".join(full)
 
         print("\n")
-        print(f"Message: {message}")
+        print(f"Original message: {message}")
         print(f"Encrypted message: {encryptedMessage}")
 
     def decrypt():
@@ -82,13 +82,13 @@ def code_generator():
         encryptedMessage = "".join(full)
 
         print("\n")
-        print(f"Message: {message}")
-        print(f"Encrypted message: {encryptedMessage}")
+        print(f"Original message: {message}")
+        print(f"Decrypted message: {encryptedMessage}")
 
-    if mode == 'encrypt' or 'e':
+    if mode in ['encrypt', 'e']:
         encrypt()
 
-    elif mode == 'decrypt' or 'd':
+    elif mode in ['decrypt', 'd']:
         decrypt()
 
-code_generator()
+caesarCipher()
